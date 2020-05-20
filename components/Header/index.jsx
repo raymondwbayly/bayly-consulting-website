5775db5000c23496d82c4b3811314ddc8fd38842
Raymond Bayly <raymond.bayly@outlook.com>
2020-05-20 14:14:01 -0400

    update package

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCompass
-} from "@fortawesome/free-solid-svg-icons";
-
+import { faCompass } from "@fortawesome/free-solid-svg-icons";
 import "./Header.scss";
 
-const Header = props => (
+const Header = (props) => (
   <Link href="/">
-    <div className="Header"><FontAwesomeIcon  icon={faCompass} /> &nbsp;{props.appTitle}</div>
+    <div className="Header">
+       <div className="logoContainer"><img src="./logo-shadow-small.png" width="50" height="50" /></div>{" "}
+      &nbsp;{props.appTitle}
+    </div>
   </Link>
 );
 
