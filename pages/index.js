80743ae27062db17759b6ec473daa5fe18a011e3
Raymond Bayly <raymond.bayly@outlook.com>
2020-05-21 12:16:43 -0400

    update the code

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
+import Paper from '@material-ui/core/Paper';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Layout from "../components/Layout";
 
@@ -9,16 +10,36 @@ const IndexView = () => (
     <Container className="container">
       <Grid container justify="center">
           <Grid item className="gridItem">
-            HELLO
+            <Paper elevation={3} className="paperContainer">
+            <div>TITLE</div>
+            <hr></hr>
+            <div>body</div>
+            <div>footer buttons</div>
+            </Paper>
           </Grid>
           <Grid item className="gridItem">
-            HELLO
+          <Paper elevation={3} className="paperContainer">
+          <div>TITLE</div>
+            <hr></hr>
+            <div>body</div>
+            <div>footer buttons</div>
+          </Paper>
           </Grid>
           <Grid item className="gridItem">
-            HELLO
+          <Paper elevation={3} className="paperContainer">
+          <div>TITLE</div>
+            <hr></hr>
+            <div>body</div>
+            <div>footer buttons</div>
+          </Paper>
           </Grid>
           <Grid item className="gridItem">
-            HELLO
+          <Paper elevation={3} className="paperContainer">
+          <div>TITLE</div>
+            <hr></hr>
+            <div>body</div>
+            <div>footer buttons</div>
+          </Paper>
           </Grid>
       </Grid>
     </Container>
