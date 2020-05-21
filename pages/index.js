7efed9a5cf8c4dcd6ad27338602a5fdebb95bbbc
Raymond Bayly <raymond.bayly@outlook.com>
2020-05-21 11:30:26 -0400

    upate navbutton

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Layout from "../components/Layout";
 
 
